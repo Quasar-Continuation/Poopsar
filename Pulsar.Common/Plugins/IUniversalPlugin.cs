1ffc2d30f6cdeca9529c14be9f562a90d904506b
King Kunta <gitlab.private273@passmail.net>
2025-10-22 10:37:00 -0500

    add a little notice to people who might mess up plugins

--- Pulsar.Common/Plugins/IUniversalPlugin.cs
+++ Pulsar.Common/Plugins/IUniversalPlugin.cs
@@ -18,6 +18,11 @@ namespace Pulsar.Common.Plugins
     {
         public bool Success { get; set; }
         public string Message { get; set; }
+
+        // I haven't tested this too much but I would try and make sure
+        // that you only use primitive data types in this or well known data types
+        // Don't send back classes that can't be deserialized due to them not being in
+        // Pulsar.Server or Pulsar.Common. Otherwise you will get serialization issues.
         public Object Data { get; set; }
         public bool ShouldUnload { get; set; }
         public string NextCommand { get; set; }
