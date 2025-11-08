5c587e2d0789072f15e4cbe307d2dec887ca1f7c
King Kunta <gitlab.private273@passmail.net>
2025-11-08 10:40:27 -0600

    fix toggle not actually being toggled on for cloning. (my bad)

--- Pulsar.Server/Forms/FrmHVNC.Designer.cs
+++ Pulsar.Server/Forms/FrmHVNC.Designer.cs
@@ -302,11 +302,11 @@ namespace Pulsar.Server.Forms
             // cLONEBROWSERPROFILEToolStripMenuItem
             // 
             this.cLONEBROWSERPROFILEToolStripMenuItem.Checked = true;
-            this.cLONEBROWSERPROFILEToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Unchecked;
+            this.cLONEBROWSERPROFILEToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
             this.cLONEBROWSERPROFILEToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
             this.cLONEBROWSERPROFILEToolStripMenuItem.Name = "cLONEBROWSERPROFILEToolStripMenuItem";
             this.cLONEBROWSERPROFILEToolStripMenuItem.Size = new System.Drawing.Size(215, 22);
-            this.cLONEBROWSERPROFILEToolStripMenuItem.Text = "DIRECT START BROWSER";
+            this.cLONEBROWSERPROFILEToolStripMenuItem.Text = "CLONE BROWSER PROFILE";
             this.cLONEBROWSERPROFILEToolStripMenuItem.Click += new System.EventHandler(this.cLONEBROWSERPROFILEToolStripMenuItem_Click);
             // 
             // sizeLabelCounter
