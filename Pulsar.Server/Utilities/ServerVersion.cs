ba60d02c162420eadc32c97a2070596b2862d434
King Kunta <gitlab.private273@passmail.net>
2025-11-10 18:47:53 -0600

    2.4.3 update

--- Pulsar.Server/Utilities/ServerVersion.cs
+++ Pulsar.Server/Utilities/ServerVersion.cs
@@ -2,7 +2,7 @@ namespace Pulsar.Server.Utilities
 {
     public static class ServerVersion
     {
-        public const string Current = "2.4.1";
+        public const string Current = "2.4.3";
 
         public static string Display => $"v{Current}";
     }
